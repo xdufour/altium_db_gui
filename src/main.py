25eb8f117402997132fcdc03c6dd0a5d377c9885
Xavier Dufour <dufourxavi@gmail.com>
2021-12-08 20:09:34 -0500

    Revert "Removed .idea"
    This reverts commit b4f1cf1c33ac3bb8bb607b623e55fffc13aa6f7a.

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ class App:
         self.tabWidget.setTabIcon(0, homeIcon)
         self.tabWidget.addTab(self.settingsWidget, '')
         self.tabWidget.setTabIcon(1, settingsIcon)
-        self.tabWidget.setIconSize(QtCore.QSize(round(self.textHeight * 2.4), round(self.textHeight * 2.4)))
+        self.tabWidget.setIconSize(QtCore.QSize(round(self.textHeight * 1.8), round(self.textHeight * 1.8)))
 
         # Settings page widgets
         self.settingsVLayout = QVBoxLayout()
